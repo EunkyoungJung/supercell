40153423ae06b2186298f0ca71b38b9a05ef76e6
MarieJung <muchogusto.lol@gmail.com>
2020-03-11 15:43:06 +0900

    Lecture 01 on March 5th

---
+++ 0227_test.py
@@ -0,0 +1,119 @@
+"""
+
+*
+**
+***
+****
+*****
+
+
+"""
+
+# for i in range(1,6):
+#     print('*'*i)
+
+
+"""
+함수
+
+입력: 문자열
+출력: 트루, 펄스
+
+"((()()()()",
+
+"""
+
+# function test(data):
+#     rlt_right = ""
+#     rlt_left = ""
+#     rlt = ""
+#     for i in data:
+#         if '(' == i:
+#             rlt_left +="("
+#             rlt_right += ")"
+#         else:
+#             rlt = rlt_left + rlt_right
+#             rlt_right = ""
+#             rlgt_left = ""
+#
+#     if rlt == data:
+#         return True
+#     else:
+#         return False
+
+"""
+
+"""
+
+# def foo(a: str, op: str, b: str)-> str:
+#     num_a = int(a)
+#     num_b = int(b)
+#     if op == "+":
+#         return str(num_a+num_b)
+#     if op == "-":
+#         return str(num_a-num_b)
+#     if op == "*":
+#         return str(num_a * num_b)
+#     if op == "/":
+#         if num_b :
+#             return str(num_a/num_b)
+#         else:
+#             return "none"
+#
+#
+#
+# r = foo("1", "+", "2")
+# print(t)
+
+# if []:
+#     print("a")
+# else:
+#     print("b")
+
+
+# a = [1, 2, 3]
+# b = (1, 2, 3)
+
+# def foo(a):
+#     return a*a
+#     print("end")
+
+
+# def foo(a, b):
+#     return {a>b: a, b>a: b}[True]
+
+# [print(i) for i in range(10)]
+
+
+# def foo(s):
+#     rlt = s.replace("   ", " ")
+#     rlt = rlt.replace("  "," ")
+#     return rlt
+#
+#
+# input = "i   am   a  boy"
+
+
+"""
+for , while
+"""
+
+# a = 10
+# print(id(10))
+# print(id(a))
+
+"""
+call of value
+call of ref
+"""
+
+# a = [1]
+# b = [1]
+# if a is b:
+#     print("same")
+# else:
+#     print("diff")
+
+"""
+oop
+"""
---
+++ lecture01_0305.py
@@ -0,0 +1,45 @@
+"""
+[memory]
+-
+
+1. native(c, c++)
+- 메모리를 개발자가 직접 제어. (해제)
+- 생산성
+- 운영체제, 펌웨어, 일부 코어 라이브러
+
+2. managed(etc)
+- 메모리를 개발자가 직접 제어 하지 않음. (해제)
+- gc, rf 0
+- 유니티(c#, js, boo)
+
+"""
+
+# if 0.1 * 0.1 == 0.01:
+#     print("true")
+# else:
+#     print("false")
+#
+# print(0.1*0.1)
+
+# import math
+#
+# def foo(n):
+#     r = math.sqrt(n)
+#     i = 0
+#     while i * i < n:
+#
+#         i+=1
+
+"""
+1. connect
+- check_connect
+- is_connect
+"""
+
+# data = input("press any key!")
+# print(data)
+
+
+
+
+print("hello")
