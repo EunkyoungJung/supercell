8ecf2d616d0542f5f4e327738f36f737e643298c
EunkyoungJung <muchogusto.lol@gmail.com>
2020-06-22 02:30:31 +0900

    Update 0601.py

--- 0601.py
+++ 0601.py
@@ -1,15 +1,25 @@
 import random
 
 
+"""
+4,  
+
+1. 남자가 동갑이거나 여자보다 최대 6살 가
+2. 1번 조건은 필수가 아니며, 없으면 나이 상관없는 사람을 할당
+3. 유효성체크에 추
+
+"""
+
 class User:
     """
-    개별 회원
     man : True
     woman : False
     """
-    def __init__(self, id=None, gender=None, prev_users=None, now_users=None):
+
+    def __init__(self, id=None, gender=None, age=None, prev_users=None, now_users=None):
         self.id = id
         self.gender = gender
+        self.age = age
         self.prev_users = prev_users if prev_users else list()
         self.now_users = now_users if now_users else list()
 
@@ -26,21 +36,14 @@ class User:
 
 
 class Generator:
-    """
-    회원 기록부
-    """
     def __init__(self):
         self.users = list()
-        self.men_count = 0
-        self.women_count = 0
 
-    def add(self, men=None, women=None):
-        """
-        회원 전체 등록
-        """
-        genders = [True] * men + [False] * women
+    def add(self, man_count=None, woman_count=None, man_age=None, woman_age=None):
+        genders = [True] * man_count + [False] * woman_count
         for gender in genders:
-            user = User(id=len(self.users), gender=gender)
+            age = random.randrange(*man_age) if gender else random.randrange(*woman_age)
+            user = User(id=len(self.users), gender=gender, age=age)
             self.users.append(user)
 
     def get_users(self):
@@ -48,9 +51,6 @@ class Generator:
 
 
 class Allocation:
-    """
-    매칭 프로세스
-    """
     @classmethod
     def __prepare_users(cls, users):
         for user in users:
@@ -59,45 +59,47 @@ class Allocation:
     @classmethod
     def __receive_users(cls, user, opposite_users):
         for opposite_user in opposite_users:
-            opposite_user.receive([user]) # 상대방의 매칭기록에 user를 추가
-        user.receive(opposite_users) # user의 매칭기록에 opposite_users를 추가
+            opposite_user.receive([user])
+        user.receive(opposite_users)
 
     @classmethod
     def __shuffle(cls, users):
-        random.shuffle(users) # users 리스트의 user들의 위치를 shuffle
+        random.shuffle(users)
 
     @classmethod
     def delivery(cls, users, quota):
-        cls.__shuffle(users)   # 균등하게 기회를 갖을 수 있도록 shuffle
-        cls.__prepare_users(users)  # 각 user의 now_users정보 초기화
+        cls.__shuffle(users)
+        cls.__prepare_users(users)
         for user in users:
             opposite_genders = list(filter(lambda x: x.gender != user.gender, users))
             new_face_users = list(filter(lambda x: x.id not in user.prev_users and user.id not in x.prev_users, opposite_genders))
-            tmp_users = sorted(new_face_users, key=lambda x: len(x.now_users))[:quota]
+            if user.gender:
+                tmp_users = sorted(new_face_users, key=lambda woman: (not bool(0 <= user.age - woman.age < 6), len(woman.now_users)))[:quota]
+            else:
+                tmp_users = sorted(new_face_users, key=lambda man: (not bool(0 <= man.age - user.age < 6), len(man.now_users)))[:quota]
             cls.__receive_users(user, tmp_users)
         users.sort(key=lambda x: x.id)
 
     @classmethod
-    def is_validate(self):
-        """
-        1. 내가 받은 카드를 상대도 받아야 됨
-        2. 이전카드, 현재카드에 동성이 있으면 안됨
-        3. 예전에 소개 받았던 이성이 현재 받으면 안됨
-        """
-        g = Generator()
-        g.add(men=20, women=10)
-        Allocation.delivery(g.get_users(), 2)
-        for user in g.get_users():
-            for now_user in user.now_users:
-                if user not in now_user.now_users:
-                    return False
-        print("Pass Case #1: 내가 받은 카드를 상대도 받아야 됨")
-
-        Allocation.__prepare_users(g.get_users())
+    def is_validate(cls, users):
+        gender_error_cnt = 0
+        match_error_cnt = 0
+        duplicate_match_error_cnt = 0
+        for user in users:
+            gender_error_cnt += len(list(filter(lambda x: x.id in user.prev_users and x.gender == user.gender, users)))
+            match_error_cnt += len(list(filter(lambda x: x.id in user.prev_users and user.id not in x.prev_users, users)))
+            duplicate_match_error_cnt += len(user.prev_users) - len(set(user.prev_users))
+        print(f"gender_error_cnt={gender_error_cnt} match_error_cnt={match_error_cnt} duplicate_match_error_cnt={duplicate_match_error_cnt}")
 
 
 
-Allocation.is_validate()
+g = Generator()
+g.add(man_count=100, woman_count=100, man_age=(20, 60), woman_age=(20, 60))
+for _ in range(3):
+    Allocation.delivery(g.get_users(), 2)
+Allocation.is_validate(g.get_users())
+for user in g.get_users():
+    print(user)
 # g = Generator()
 # g.add(men=20, women=10)
 # for _ in range(3):
